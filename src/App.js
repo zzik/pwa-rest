041dfeafbd8bee7a982287153b2efafbbd8ddeff
Edin Basic <edin.base@outlook.com>
2021-09-03 13:16:37 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
 
 function App() {
+  const [Value, setValue] = React.useState()
+  const [Data, setData] = React.useState()
+
+  // Get Data from API
+  const getData = async (location) => {
+    const response = await fetch(
+      `https://community-open-weather-map.p.rapidapi.com/weather?q=${Value}&units=metric`,
+      {
+        method: "GET",
+        headers: {
+          "x-rapidapi-host": "community-open-weather-map.p.rapidapi.com",
+          "x-rapidapi-key":
+            "b8442541a0msh3e3a3e1ed20cf13p17391cjsn6db72ae58944",
+        },
+      }
+    )
+
+    const data = await response.json()
+
+    response.status === 200 && setData(data);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app">
+      <header className="header">
+        <input
+          type="search"
+          className="input"
+          placeholder="Search"
+          onChange={(e) => setValue(e.target.value)}
+        />
+
+        <button onClick={() => getData(Value)}>Search</button>
       </header>
+
+      <main>
+        {Data ? (
+          <section>
+            <div className="group">
+              <strong>Location: </strong>
+              <span>{Data.name}</span>
+            </div>
+
+            <div className="group">
+              <strong>Temperature: </strong>
+              <span>{Data.main.temp} &#176;C</span>
+            </div>
+          </section>
+        ) : (
+          <h2>Search by location</h2>
+        )}
+      </main>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
